87624d94f591131f01e9512a15483a8c2a6735f5
pneff <pneff@unicon.net>
2018-09-08 07:57:25 -0700

    6-124 admin user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    session[:user_id] = user.id
+
     if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
       redirect_to user_path(user)
     else
