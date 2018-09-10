d6f67686ea162eb09486287498aa42e2c86b6a63
pneff <pneff@unicon.net>
2018-09-10 10:51:32 -0700

    133 Integration test

--- README.md
+++ README.md
@@ -22,3 +22,6 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+# MISC
+Left off 133 12:50
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,30 @@
 class CategoriesController < ApplicationController
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Category was created successfully"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
 
   end
 
   def index
+    @categories = Category.all
 
   end
 
   def show
 
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
